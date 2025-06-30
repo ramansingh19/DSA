edc81ec87984ffe3a21f60927dfd2816465e426b
ramansingh19 <raman.01kumar1@gmail.com>
2025-06-30 18:33:09 +0530

    palindromecheck

---
+++ VariableAndDataType/Basics/palindromeNumbers.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class palindromeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome.");
+        } else {
+            System.out.println(num + " is not a palindrome.");
+        }
+
+        sc.close();
+    }
+
+    public static boolean isPalindrome(int num) {
+        int original = num;
+        int reversed = 0;
+
+        while (num > 0) {
+            int digit = num % 10;              // get last digit
+            reversed = reversed * 10 + digit;  // build reversed number
+            num = num / 10;                    // remove last digit
+        }
+
+        return original == reversed;
+    }
+}
+
