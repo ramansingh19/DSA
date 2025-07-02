9fefe4c0c4742e53b8a6b0174ed162479b45dd20
ramansingh19 <raman.01kumar1@gmail.com>
2025-07-02 23:33:20 +0530

    a

---
+++ VariableAndDataType/Basics/Pattern/starPattern.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class starPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        for ( i=str; i<=5; i++){
+            System.out.println(i);
+        }
+    }
+}
