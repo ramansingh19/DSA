fe7f07688d477f47801724f54a38edde37106914
ramansingh19 <raman.01kumar1@gmail.com>
2025-07-19 07:39:23 +0530

    using prefix method to find the value using 2 loop

--- VariableAndDataType/Basics/Array/Binarysearch/maxsubarraySum.java
+++ VariableAndDataType/Basics/Array/Binarysearch/maxsubarraySum.java
@@ -2,18 +2,26 @@ package Array.Binarysearch;
 import java.util.*;
 public class maxsubarraySum {
 
+    //0(nsqrof2)
+
     public static void maxSubrraysum(int arr[]){
         int maxSum = Integer.MIN_VALUE;
         int minSum = Integer.MAX_VALUE;
+        int prefix[] = new int[arr.length];
+
+        //use this formula to find the max sum
+        prefix[0] = arr[0];
+        for (int i=1; i<prefix.length; i++){
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
+        //loop to read the values
         for (int i=0; i<arr.length; i++){
             int start = i;
             for (int j=i; j<arr.length; j++){
                 int end = j;
-                int sum = 0;
-                for (int k =start ; k<=end ; k++){
-                    System.out.print(arr[k] + " ");
-                    sum = sum + arr[k];
-                }
+                //tofind the
+                int sum = start ==0 ? prefix[end] : prefix[end] - prefix[start-1];
                 System.out.print("sum :" + sum);
                 System.out.println();
                 if (sum > maxSum){
@@ -28,8 +36,8 @@ public class maxsubarraySum {
         }
         System.out.println("maxsum "+ maxSum);
         System.out.println("minsum"+ minSum);
-
     }
+
 
     public static void main(String[] args) {
         int arr[] = {1,-2,6,-1,3};
