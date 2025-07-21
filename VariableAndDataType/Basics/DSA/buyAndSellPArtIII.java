d3d0686026c380ee6076bb2a7d47d6fe440e5ca6
ramansingh19 <raman.01kumar1@gmail.com>
2025-07-21 17:42:03 +0530

    buy and sell part II

---
+++ VariableAndDataType/Basics/DSA/buyAndSellPArtIII.java
@@ -0,0 +1,12 @@
+package DSA;
+
+public class buyAndSellPArtIII {
+
+    public static int buyAndSellPArtIII(int prices[]){
+
+    }
+
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+    }
+}
---
+++ VariableAndDataType/Basics/DSA/buyandSellII.java
@@ -0,0 +1,20 @@
+package DSA;
+
+public class buyandSellII {
+
+    public static int buyAndSellPArtII(int prices[]){
+        int profit = 0;
+        for (int i=1; i<prices.length; i++){
+            if (prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return  profit;
+    }
+
+    public static void main(String[] args) {
+
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println("PRofit will be : "+ buyAndSellPArtII(prices));
+    }
+}
