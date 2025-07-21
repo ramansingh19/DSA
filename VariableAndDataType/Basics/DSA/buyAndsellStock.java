d3e6f230ed1050df9eb753670167f6e5de170eea
ramansingh19 <raman.01kumar1@gmail.com>
2025-07-21 11:01:56 +0530

    best day to buy and sell

---
+++ VariableAndDataType/Basics/DSA/buyAndsellStock.java
@@ -0,0 +1,26 @@
+package DSA;
+import java.util.*;
+
+public class buyAndsellStock {
+
+    public static int buyAndSellSTock(int prices[] ){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i=0; i<prices.length; i++){
+            if (buyPrice < prices[i]){//Profit
+                int profit = prices[i] - buyPrice;//today profit
+                maxProfit = Math.max(maxProfit , profit);
+            }else  {
+                buyPrice = prices[i];
+            }
+        }
+        return  maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println("if we sell then profit will be :"+ buyAndSellSTock(prices));
+
+    }
+}
