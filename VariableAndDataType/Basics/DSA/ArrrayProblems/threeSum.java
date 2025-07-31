a213c96d032ea78fa9758ca6572ac398e5234fe6
ramansingh19 <raman.01kumar1@gmail.com>
2025-07-31 18:03:45 +0530

    s

--- VariableAndDataType/Basics/DSA/ArrrayProblems/threeSum.java
+++ VariableAndDataType/Basics/DSA/ArrrayProblems/threeSum.java
@@ -1,40 +1,68 @@
 package DSA.ArrrayProblems;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class threeSum {
 
+
+    //using Brute force Method;
     public List<List<Integer>> threesum(int nums[]){
-        Arrays.sort(nums);
-        List<List<Integer>>result = new ArrayList<>();
+        int n = nums.length;
+        Set<List<Integer>>set = new HashSet<>();
+        List<List<Integer>>ans = new ArrayList<>();
 
-        for (int i=0; i< nums.length-2; i++){
-            if (i>0 && nums[i]==nums[i-1]) continue;
 
-            int left = i+1;
-            int right = nums.length-1;
-
-            while (left < right){
-                int sum = nums[i] + nums[left]+nums[right];
-
-                if(sum == 0){
-                    result.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
-
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++; right--;
-                }else  if(sum<0){
-                    left++;
-                }else {
-                    right--;
+        for (int i=0; i<n; i++){ 
+            for (int j=i+1; j<n; j++){
+                for (int k=j+1; k<n; k++){
+                    if (nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> triplet = Arrays.asList(nums[i] , nums[j] , nums[k]);
+                        Collections.sort(triplet);
+                        set.add(triplet);
+                    }
                 }
             }
         }
-        return result;
+        ans.addAll(set);
+        return ans;
+
     }
+
+    //using hashset method
+    
+
+
+
+    //using twoPointer Method
+//    public List<List<Integer>> threesum(int nums[]){
+//        Arrays.sort(nums);
+//        List<List<Integer>>result = new ArrayList<>();
+//
+//        for (int i=0; i< nums.length-2; i++){
+//            if (i>0 && nums[i]==nums[i-1]) continue;
+//
+//            int left = i+1;
+//            int right = nums.length-1;
+//
+//            while (left < right){
+//                int sum = nums[i] + nums[left]+nums[right];
+//
+//                if(sum == 0){
+//                    result.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
+//
+//                    while (left < right && nums[left] == nums[left + 1]) left++;
+//                    while (left < right && nums[right] == nums[right - 1]) right--;
+//
+//                    left++; right--;
+//                }else  if(sum<0){
+//                    left++;
+//                }else {
+//                    right--;
+//                }
+//            }
+//        }
+//        return result;
+//    }
 
     public static void main(String[] args) {
         int nums[] = {-1, 0, 1, 2, -1, -4};
