4df1350a571fbdf9df95ca2f146e5997a42de4a2
ramansingh19 <raman.01kumar1@gmail.com>
2025-08-11 18:14:18 +0530

    error solve for index bounding exxcedds

--- VariableAndDataType/Basics/DSA/searchinSortedMatrix.java
+++ VariableAndDataType/Basics/DSA/searchinSortedMatrix.java
@@ -4,15 +4,37 @@ public class searchinSortedMatrix {
 
 
     //Brute Force approach
+//    public static boolean searchSortedMatrix(int matrix[][], int target){
+//        for (int i=0; i< matrix.length; i++){
+//            for (int j=0; j<matrix[0].length; j++){
+//                if (matrix[i][j] == target){
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
+    //Staircase search approach
+
     public static boolean searchSortedMatrix(int matrix[][], int target){
-        for (int i=0; i< matrix.length; i++){
-            for (int j=0; j<matrix[0].length; j++){
-                if (matrix[i][j] == target){
+            int row = 0;
+            int col = matrix[0].length-1;
+
+            while (row < matrix.length && col >= 0){
+                if (matrix[row][col] == target){
+                    System.out.print("Found the target at (" + row + ", "+ col +")");
                     return true;
                 }
+                else if (target < matrix[row][col]){
+                    col--;
+                }
+                else{
+                    row++;
+                }
             }
-        }
-        return false;
+        System.out.println("target not found");
+            return false;
     }
 
     public static void main(String[] args) {
@@ -20,13 +42,14 @@ public class searchinSortedMatrix {
                              {15, 25, 35, 45},
                                   {27, 29, 37, 48},
                                         {32,33,39,50}};
-        int target = 90;
+        int target = 33;
+        searchSortedMatrix(matrix , target);
 
-        if (searchSortedMatrix(matrix, target)){
-            System.out.println("Element found : "+  target);
-        }else{
-            System.out.println("Element not found");
-        }
+//        if (searchSortedMatrix(matrix, target)){
+//            System.out.println("Element found : "+  target);
+//        }else{
+//            System.out.println("Element not found");
+//        }
     }
 
 }
