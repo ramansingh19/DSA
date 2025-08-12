bc68620167b252c89e46703ff56b777cfd99e5bd
ramansingh19 <raman.01kumar1@gmail.com>
2025-08-12 16:16:14 +0530

    Sum of the number of the any coln present in the array

---
+++ VariableAndDataType/Basics/TwoDArray/SumofNumbers.java
@@ -0,0 +1,20 @@
+package TwoDArray;
+
+public class SumofNumbers {
+
+    public static void SumofNumber(int arr[][]){
+        int sum = 0;
+        for (int i=0; i< arr[0].length; i++){
+           sum += arr[1][i];
+        }
+        System.out.println("Sum of numbers :"+ sum);
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = {{1,4,9},
+                            {11,4,3},
+                                {2,2,3}};
+
+        SumofNumber(arr);
+    }
+}
