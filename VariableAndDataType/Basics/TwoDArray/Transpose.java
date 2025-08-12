7e3c971a7a614941a385d106772a53fd857cbf5e
ramansingh19 <raman.01kumar1@gmail.com>
2025-08-12 20:57:03 +0530

    h

---
+++ VariableAndDataType/Basics/TwoDArray/Transpose.java
@@ -0,0 +1,52 @@
+package TwoDArray;
+
+public class Transpose {
+
+    public static void main(String[] args) {
+        int arr[][] = {{2,4,5,8},
+                            {9,4,3,3}};
+
+        int row = 2;
+        int coln = 3;
+
+        int[][] trasposed = new int[coln][row];//swapped number
+        for (int i=0; i< arr.length; i++){
+            for (int j=0; j<arr[0].length; j++){
+                trasposed[j][i]  = arr[i][j];
+            }
+        }
+
+    }
+
+    public static
+
+
+//    public static int[][] transpose(int arr[][]){
+//        int row = arr.length;
+//        int coln = arr[0].length;
+//
+//        //new matrix with swapped dimension
+//        int[][] transposed = new int[coln][row];
+//
+//        for (int i=0; i<row; i++){
+//            for (int j=0; j<coln; j++){
+//                transposed[j][i] = arr[i][j];//swap row and column
+//            }
+//        }
+//        return transposed;
+//    }
+//
+//    public static void main(String[] args) {
+//        int arr[][] = {{2,4,5,8},
+//                            {9,4,3,3}};
+//
+//        int[][] result = transpose(arr);
+//
+//        for (int i=0; i<result.length; i++){
+//            for (int j=0; j<result[0].length; j++){
+//                System.out.print(result[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+}
