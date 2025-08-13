6f50081c82b6cd07a13399a5912f385bff262e84
ramansingh19 <raman.01kumar1@gmail.com>
2025-08-13 10:17:28 +0530

    solving

--- VariableAndDataType/Basics/TwoDArray/Transpose.java
+++ VariableAndDataType/Basics/TwoDArray/Transpose.java
@@ -7,18 +7,27 @@ public class Transpose {
                             {9,4,3,3}};
 
         int row = 2;
-        int coln = 3;
+        int coln = 4;
 
         int[][] trasposed = new int[coln][row];//swapped number
-        for (int i=0; i< arr.length; i++){
-            for (int j=0; j<arr[0].length; j++){
+        for (int i=0; i< row; i++){
+            for (int j=0; j< coln; j++){
                 trasposed[j][i]  = arr[i][j];
             }
         }
-
+        Printarr(trasposed);
     }
 
-    public static
+   public static void Printarr(int arr[][]){
+       System.out.println("The matrix is :");
+       for (int i=0; i< arr.length; i++){
+           for (int j=0; j<arr[0].length; j++){
+               System.out.print(arr[i][j] + " ");
+           }
+           System.out.println();
+       }
+
+   }
 
 
 //    public static int[][] transpose(int arr[][]){
