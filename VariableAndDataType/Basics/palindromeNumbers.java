cc9eaa99545d0ff52f94453167faa434d0b2c567
ramansingh19 <raman.01kumar1@gmail.com>
2025-08-16 17:09:00 +0530

    palindrome

--- VariableAndDataType/Basics/palindromeNumbers.java
+++ VariableAndDataType/Basics/palindromeNumbers.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 
 public class palindromeNumbers {
+
+
+    public static boolean isPalindrome(int num) {
+        int original = num;
+        int reversed = 0;
+
+        while (num > 0) {
+            int digit = num % 10;              // get last digit
+            reversed = reversed * 10 + digit;  // build reversed number
+            num = num / 10;                    // remove last digit
+        }
+
+        return original == reversed;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -14,19 +30,6 @@ public class palindromeNumbers {
         }
 
         sc.close();
-    }
-
-    public static boolean isPalindrome(int num) {
-        int original = num;
-        int reversed = 0;
-
-        while (num > 0) {
-            int digit = num % 10;              // get last digit
-            reversed = reversed * 10 + digit;  // build reversed number
-            num = num / 10;                    // remove last digit
-        }
-
-        return original == reversed;
     }
 }
 
