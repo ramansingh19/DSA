d6b3521eadac5968db9372cd6d6f2004512228c0
ramansingh19 <raman.01kumar1@gmail.com>
2025-08-20 12:00:46 +0530

    duplicate string

---
+++ VariableAndDataType/Basics/String/Duplicate.java
@@ -0,0 +1,22 @@
+package String;
+
+public class Duplicate {
+
+    public static void getDuplicate(String str) {
+        String count = " ";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (count.indexOf(ch) == -1) {
+                count += ch;
+            }
+        }
+        System.out.println("After removing all duplicate : " + count);
+    }
+
+    public static void main(String[] args) {
+        String str = "Disco Disco ";
+        getDuplicate(str);
+
+    }
+}
