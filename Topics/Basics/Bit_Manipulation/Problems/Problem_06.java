d4f514e757ba287e182dd9ff807e479ce06838ed
ramansingh19 <raman.01kumar1@gmail.com>
2025-09-03 17:52:22 +0530

    bit manipulation

---
+++ Topics/Basics/Bit_Manipulation/Problems/Problem_06.java
@@ -0,0 +1,16 @@
+package Bit_Manipulation.Problems;
+
+public class Problem_06 {
+
+    public static boolean powOfTwo(int n){
+        if (n <= 0){
+            return false;
+        }
+        return (n & (n-1))==0;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(powOfTwo(64));
+    }
+}
