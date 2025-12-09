36ce2996599c3a2936418528a0de344273a2e4cd
ramansingh19 <raman.01kumar1@gmail.com>
2025-12-09 23:59:49 +0530

    dd

--- Topics/Basics/RevisionPatternsProblems/InvertedNumberPyramid.java
+++ Topics/Basics/RevisionPatternsProblems/InvertedNumberPyramid.java
@@ -2,6 +2,11 @@ package RevisionPatternsProblems;
 
 public class InvertedNumberPyramid {
     public static void main(String[] args) {
-
+            for (int i=5; i>=0; i--){
+                for (int j=0; j<=i-1; j++){
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
     }
 }
