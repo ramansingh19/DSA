7e9e66f6b3dd4c1bc9f2de2e2bae8ea32cf8081e
ramansingh19 <raman.01kumar1@gmail.com>
2025-12-10 00:21:51 +0530

    ss

---
+++ Topics/Basics/RevisionPatternsProblems/SquarePattern.java
@@ -0,0 +1,12 @@
+package RevisionPatternsProblems;
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++){
+            for (int j=5; j>=1; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
