e27681fa691575c5a14e4e48ec225003e38e2a37
ramansingh19 <raman.01kumar1@gmail.com>
2025-12-10 23:24:06 +0530

    s

--- Topics/Basics/Array/RevisionArray/LinearSearch.java
+++ Topics/Basics/Array/RevisionArray/LinearSearch.java
@@ -1,6 +1,14 @@
 package Array.RevisionArray;
 
 public class LinearSearch {
+    public static int  name(String str[], String word){
+        for (int i=0; i< str.length; i++){
+            if (str[i] == word){
+                return i;
+            }
+        }
+        return -1;
+    }
     public static int findNumber(int number[], int key){
         for (int i=0; i<=number.length-1; i++){
             if (number[i] == key){
@@ -11,14 +19,23 @@ public class LinearSearch {
     }
     public static void main(String[] args) {
         int number[] = {2,3,4,5,6,7,8,9,10};
+        String str[] = {"raman", "aman"};
+        String word = "aman";
         int key = 5;
 
-        int index = findNumber(number , key);
-        if (index == -1){
+        int findname = name(str , word);
+        if (findname == -1){
             System.out.print("not found ");
         }else {
-            System.out.print("found at index " + index);
+            System.out.print("found at index "+ findname);
         }
-        System.out.println();
+
+//        int index = findNumber(number , key);
+//        if (index == -1){
+//            System.out.print("not found ");
+//        }else {
+//            System.out.print("found at index " + index);
+//        }
+//        System.out.println();
     }
 }
