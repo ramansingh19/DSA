4276fb49380e01bc2718bab8e85e37bce98bd04d
ramansingh19 <raman.01kumar1@gmail.com>
2025-12-17 23:40:42 +0530

    pair of array and subarray

---
+++ Topics/Basics/Array/RevisionArray/pairinArray.java
@@ -0,0 +1,17 @@
+package Array.RevisionArray;
+
+public class pairinArray {
+    public static void pairinArray(int arr[]){
+        for (int i=0; i<arr.length; i++){
+            for (int j=i+1; j<arr.length; j++){
+                System.out.print("(" + arr[i] + "," + arr[j] + ")");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10};
+        pairinArray(arr);
+    }
+}
