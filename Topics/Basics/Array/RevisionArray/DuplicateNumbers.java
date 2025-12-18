eed4b5e34e5098f741d77f5ce585020dd2ab2d16
ramansingh19 <raman.01kumar1@gmail.com>
2025-12-18 23:43:09 +0530

    s

---
+++ Topics/Basics/Array/RevisionArray/DuplicateNumbers.java
@@ -0,0 +1,21 @@
+package Array.RevisionArray;
+
+public class DuplicateNumbers {
+    public static void DuplicateNumber(int arr[]) {
+        int num = 0;
+
+        for (int i=0; i<arr.length; i++){
+            for (int j=i; j<arr.length; j++){
+                if (arr[i] == arr[j] && i != j){
+                    System.out.println("Duplicate " + arr[j]);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,2,4,10};
+        DuplicateNumber(arr);
+    }
+}
